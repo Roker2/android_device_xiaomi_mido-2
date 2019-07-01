a5389c16c6a98ffa77e1be8c90f2d2ebb69a24a6
Danct12 <danct12@disroot.org>
2019-07-02 01:28:45 +0700

    device.mk: added media_compat_layer
    Signed-off-by: Danct12 <danct12@disroot.org>

--- device.mk
+++ device.mk
@@ -281,6 +281,7 @@ PRODUCT_PACKAGES += \
     libcameraservice_32 \
     libdroidmedia_32 \
     libcamera_compat_layer_32 \
+    libmedia_compat_layer_32 \
     libui_compat_layer_32 \
     libsf_compat_layer_32 \
     minimediaservice \
